b533478e784684dd4b9da305fcae4ff001bf4c98
Femke Knottenbelt <femknotgithub@gmail.com>
2021-05-07 07:51:03 +0200

    Set up routes for first resource

---
+++ routes/bootcamps.js
@@ -0,0 +1,24 @@
+const express = require('express'); // koppel aan express
+const router = express.Router(); 
+
+router.get('/', (req, res) => {
+    res.status(200).json({ succes: true, msg: 'Show all bootcamps' });
+  });
+  
+router.get('/:id', (req, res) => {
+    res.status(200).json({ succes: true, msg: `Show single bootcamp ${req.params.id}` });
+});
+
+router.post('/', (req, res) => {
+    res.status(200).json({ succes: true, msg: 'Create new bootcamp' });
+});
+
+router.put('/:id', (req, res) => {
+res.status(200).json({ succes: true, msg: `Update bootcamp ${req.params.id}` });
+});  
+
+router.delete('/:id', (req, res) => {
+    res.status(200).json({ succes: true, msg: `Delete bootcamp ${req.params.id}` });
+});
+  
+module.exports = router;

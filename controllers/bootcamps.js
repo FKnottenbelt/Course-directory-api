09aaff2823c8478e820368a0f91c1e4932c08410
Femke Knottenbelt <femknotgithub@gmail.com>
2021-07-24 09:39:13 +0200

    Add validations to error handler

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -9,8 +9,8 @@ exports.getBootcamps = async (req, res, next) => {
     const bootcamps = await Bootcamp.find();
 
     res.status(200).json({ succes: true, count: bootcamps.length, data: bootcamps});  
-  } catch (error) {
-    res.status(400).json({ success: false});
+  } catch (err) {
+    next(err);
   }
 }
 
@@ -28,7 +28,7 @@ exports.getBootcamp = async (req, res, next) => {
     res.status(200).json({ succes: true, data:
        bootcamp});    
   } catch (err) {
-    next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
+    next(err);
   }
 }
 
@@ -43,8 +43,8 @@ exports.createBootcamp = async (req, res, next) => {
       success: true,
       data: bootcamp
     });
-  } catch {
-    res.status(400).json({success: false});
+  } catch (err) {
+    next(err);
   }  
 }
 
@@ -59,12 +59,12 @@ exports.updateBootcamp = async (req, res, next) => {
       });
   
     if (!bootcamp) {
-      return res.status(400).json({ success: false});
+      return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
     }
   
     res.status(200).json({ succes: true, data: bootcamp });    
-  } catch (error) {
-    res.status(400).json({success: false});
+  } catch (err) {
+    next(err);
   }
 
 };
@@ -77,11 +77,11 @@ exports.deleteBootcamp = async (req, res, next) => {
     const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
   
     if (!bootcamp) {
-      return res.status(400).json({ success: false});
+      return next(new ErrorResponse(`Bootcamp not found with id of ${req.params.id}`, 404));
     }
   
     res.status(200).json({ succes: true, data: {}} );    
-  } catch (error) {
-    res.status(400).json({success: false});
+  } catch (err) {
+    next(err);
   }
 };
--- middleware/error.js
+++ middleware/error.js
@@ -1,9 +1,41 @@
+const ErrorResponse = require("../utils/errorResponse");
+
 const errorHandler =  (err, req, res, next) => {
+  let error = { ...err };  
+
   // log to console for dev
-  console.log(err.stack.red);
-  res.status(err.statusCode || 500).json({ 
+  console.log(`Error in middleware/error.js errorHandler`.brightRed, error); 
+  console.log(`Error in middleware/error.js errorHandler ${err.errmsg}`.brightRed ); 
+
+  error.message = err.message;
+   
+  // Mongoose bad ObjectId
+  if (err.name === 'CastError') {  // id not formatted properly
+    const message = `Resource not found with id of ${err.errmsg}`;
+   
+    console.log(`Custom Response Error message`.red, message);
+    error = new ErrorResponse(message, 404); 
+  }
+
+  // Mongoose duplicate key
+  if (err.name === 'MongoError' && err.code === 11000) { 
+    const message = `Record with this id allready exists - ${err.errmsg.substring(err.errmsg.indexOf('{'))}`;
+   
+    console.log(`Custom Response Error message`.red, message);
+    error = new ErrorResponse(message, 400); // bad request 
+  }
+
+  // Mongoose validation error
+  if (err.name === 'ValidationError') {
+    const message = Object.values(err.errors).map(val => val.message).join(', ');
+   
+    console.log(`Custom Response Error message`.red, message);
+    error = new ErrorResponse(message, 400); // bad request 
+  }
+
+  res.status(error.statusCode || 500).json({ 
     success: false,
-    error: err.message || 'Sever Error'
+    error: error.message || 'Sever Error'
   });
 }
 

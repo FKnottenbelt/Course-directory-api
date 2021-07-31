6bd0777cff7db8a7f1be94fcf9f09f7a033f0f1f
Femke Knottenbelt <femknotgithub@gmail.com>
2021-07-31 10:49:55 +0200

    Add mongoose hook to add slug for name

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const BootcampSchema = new mongoose.Schema(
   {
@@ -98,5 +99,11 @@ const BootcampSchema = new mongoose.Schema(
       default: Date.now
     }
   });
+
+// create bootcamp slug from name
+BootcampSchema.pre('save', function(next) {
+  this.slug = slugify(this.name, { lower: true});
+  next();
+});
 
 module.exports = mongoose.model('Bootcamp', BootcampSchema);

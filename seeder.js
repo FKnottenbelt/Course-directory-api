0300b1332e97537dc951cf2a0bfc75293cc0f9ae
Femke Knottenbelt <femknotgithub@gmail.com>
2021-09-04 11:29:37 +0200

    Update dependencies

--- seeder.js
+++ seeder.js
@@ -12,8 +12,7 @@ const Bootcamp = require('./models/Bootcamp');
 // connect to DB
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
+  autoIndex: true,
   useUnifiedTopology: true
 });
 

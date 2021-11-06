906da9e91508a5c0f5f8724b67de5f83f99bc008
Femke Knottenbelt <femknotgithub@gmail.com>
2021-11-06 09:44:44 +0100

    Add new and get single courses

--- routes/courses.js
+++ routes/courses.js
@@ -2,10 +2,16 @@ const express = require('express');
 const { model } = require('mongoose');
 const {
   getCourses,
+  getCourse,
+  addCourse
   } = require('../controllers/courses');
 
 const router = express.Router({ mergeParams: true}); 
 
-router.route('/').get(getCourses);
+router
+  .route('/')
+  .get(getCourses)
+  .post(addCourse);
+router.route('/:id').get(getCourse);
 
 module.exports = router;
